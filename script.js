c6ee032b363c70604398ecc519b8371e3dc00cb0
SehrGuto <pavani.augusto@gmail.com>
2024-02-02 10:56:55 -0500

    se agrega la conversióna  Kelvin

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ function convertTemperature() {
 
     // una variable para almacenar el resultado
     let convertedTemperature;
+    let convertedTemperature2;
 
     //dependiendo de la unidad de temperatura usada en el formulario realizar la conversión
     if (unit === 'celsius') {
-        // Convertir de Celsius a Fahrenheit y agregar al html
+        // Convertir de Celsius a Fahrenheit y a Kelvin y agregar al html
         convertedTemperature = (temperature * 9/5) + 32;
-        resultDiv.innerHTML = temperature + ' grados Celsius son ' + convertedTemperature.toFixed(2) + ' grados Fahrenheit.';
-    } else {
-        // Convertir de Fahrenheit a Celsius y agregar al html
+        convertedTemperature2 = (temperature + 273.15);
+        resultDiv.innerHTML = temperature + ' grados Celsius son ' + convertedTemperature.toFixed(2) + ' grados Fahrenheit. y ' + convertedTemperature2.toFixed(2) + ' Kelvin.';
+    } else if (unit === 'fahrenheit'){
+        // Convertir de Fahrenheit a Celsius y a kelvin y agregar al html
         convertedTemperature = (temperature - 32) * 5/9;
-        resultDiv.innerHTML = temperature + ' grados Fahrenheit son ' + convertedTemperature.toFixed(2) + ' grados Celsius.';
+        convertedTemperature2 = (temperature + 459.67) * 5/9;
+        resultDiv.innerHTML = temperature + ' grados Fahrenheit son ' + convertedTemperature.toFixed(2) + ' grados Celsius. y ' + convertedTemperature2.toFixed(2) + ' Kelvin.';
     }
 }
